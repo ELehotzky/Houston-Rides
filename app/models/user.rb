839510ac3c04e91d24f6e7ae3ce39a342d9da1cb
Ray <ray.rodz.88@gmail.com>
2018-08-22 17:12:21 -0500

    user#show

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,13 @@ class User < ApplicationRecord
 	has_secure_password
 
 	has_many :bikes
-	has_many :types, through: :bike 
-	has_many :trails, through: :rating
-	has_many :trails, through: :type
-	has_many :events, through: :sign_up
-	has_many :events, through: :type
+	has_many :ratings
+	has_many :sign_ups
+	has_many :types
+	has_many :types, through: :bikes 
+	has_many :trails, through: :ratings
+	has_many :trails, through: :types
+	has_many :events, through: :sign_ups
+	has_many :events, through: :types
 
 end
