e1b6fed962eec7a12b89d07bcd6edcab52e1a2bb
ELehotzky <Lehotzky.Erica@gmail.com>
2018-08-23 10:01:23 -0500

    routed root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  
+
 	resources :users, :bikes, :events, :trails, :types, :ratings, :sessions, :sign_ups
+	root "users#index"
 	get "/signup", to: "users#new"
 	get "/login", to: "sessions#new"
 	post "sessions", to: "sessions#create"
